29c4e7328110e9aef6c61d1e2f829a60432877cc
Nino Hägglund <ninomh99@gmail.com>
2025-09-08 21:35:06 +0200

    Add Coupons table and update database configuration
    - Implemented migration class `AddCouponToDb` to create a new `Coupons` table with properties for `CouponId`, `CouponCode`, `Discount`, and `MinAmount`.
    
    - Updated `AppDbContextModelSnapshot` to include the new table structure.
    
    - Configured `AppDbContext` in `Program.cs` with a SQL Server connection string.

--- PCBuilder.Services.TestAPI/Program.cs
+++ PCBuilder.Services.TestAPI/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using PCBuilder.Services.TestAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
