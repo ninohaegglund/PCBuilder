83a17b3b5643935f9c88f96169eb847fddb17f02
Nino Hägglund <ninomh99@gmail.com>
2025-09-08 19:38:01 +0200

    Add project files.

---
+++ PCBuilder.Services.TestAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PCBuilder.Services.TestAPI.Models;
+
+namespace PCBuilder.Services.TestAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Coupon> Coupons { get; set; }
+}
---
+++ PCBuilder.Services.TestAPI/Models/Coupon.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PCBuilder.Services.TestAPI.Models;
+
+public class Coupon
+{
+    [Key]
+    public int CouponId { get; set; }
+    [Required]
+    public string CouponCode { get; set; }
+    [Required]
+    public double Discount { get; set; }
+    public int MinAmount { get; set; }
+}
