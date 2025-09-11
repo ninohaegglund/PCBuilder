aec0a8227d034b91f1270f71c489dca7acba9408
Nino Hägglund <ninomh99@gmail.com>
2025-09-11 17:37:23 +0200

    add CouponService with CRUD API integration and apply new Bootstrap theme

--- PCBuilder.Web/Program.cs
+++ PCBuilder.Web/Program.cs
@@ -1,7 +1,18 @@
+using PCBuilder.Web.Service;
+using PCBuilder.Web.Service.IService;
+using PCBuilder.Web.Utility;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<ICouponService,CouponService>();
+SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"]!;
+
+builder.Services.AddScoped<IBaseService, BaseService>();
+builder.Services.AddScoped<ICouponService, CouponService>();
 
 var app = builder.Build();
 
