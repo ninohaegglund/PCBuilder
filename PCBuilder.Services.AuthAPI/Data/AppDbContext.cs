00a7e86bfff12a09b8e30b00816740d59451df3d
Nino Hägglund <ninomh99@gmail.com>
2025-09-29 21:28:36 +0200

    Add Identity integration and user management features
    - Introduced `AppDbContext` for ASP.NET Core Identity.
    
    - Updated `ApplicationUser` class to include `Name` property.
    
    - Configured services in `Program.cs` for database context and identity.

---
+++ PCBuilder.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PCBuilder.Services.AuthAPI.Models;
+
+
+namespace PCBuilder.Services.AuthAPI.Data;
+
+public class AppDbContext : IdentityDbContext<ApplicationUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+    }
+}
---
+++ PCBuilder.Services.AuthAPI/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PCBuilder.Services.AuthAPI.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string? Name { get; set; }
+}
