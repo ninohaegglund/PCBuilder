830cfc522f1c89c225a2227d0b3b49717747d3cc
ninohaegglund <ninomh99@gmail.com>
2026-03-27 09:30:40 +0100

    Fix customerDTO

--- PCBuilder.Services.CustomerAPI/DTO/CustomerDTO.cs
+++ PCBuilder.Services.CustomerAPI/DTO/CustomerDTO.cs
@@ -4,5 +4,5 @@ public class CustomerDTO
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
 }
--- PCBuilder.Services.CustomerAPI/Models/Customer.cs
+++ PCBuilder.Services.CustomerAPI/Models/Customer.cs
@@ -3,7 +3,6 @@
 public class Customer
 {
     public int Id { get; set; }
-    public int OrderId { get; set; }
     public string Name { get; set; } = string.Empty;
     public string ImageUrl { get; set; } = string.Empty;
 }
